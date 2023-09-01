2baa99bf3130c662e38238a7887c1436f14e1c1c
LEE <lee@LEEui-MacBookAir.local>
2023-09-01 14:33:04 +0900

    init

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -1,7 +1,25 @@
+import Trophy from "../../public/trophyicon.svg";
+import Lookbook from "../../public/lookbookicon.svg";
+import Chat from "../../public/chaticon.svg";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import NavBarButton from "./navbarButton";
+
 export default function NavBar() {
     return (
         <div className="absolute bottom-0 flex justify-around items-center w-[100%] h-[56px] bg-slate-500">
-            navBar
+            <Link href="/challenge">
+                <NavBarButton type={"challenge"} />
+            </Link>
+            <Link href="/lookbook">
+                <NavBarButton type={"lookbook"} />
+            </Link>
+            <Link href="/feedback">
+                <NavBarButton type={"feedback"} />
+            </Link>
+            <Link href="/loading">
+                <NavBarButton type={"loading"} />
+            </Link>
         </div>
     );
 }
---
+++ app/lookbook/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../Components/navbar";
+
+export default function Lookbook() {
+    return (
+        <main className="flex flex-col justify-center items-center w-[100%] h-[100%] bg-black text-white">
+            <h1>Lookbook</h1>
+            <NavBar />
+        </main>
+    );
+}
