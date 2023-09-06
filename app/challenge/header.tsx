e8eaceaaf359cc876423fe35e4863d357b7cbbec
LEE <eslee850@gmail.com>
2023-09-07 03:12:26 +0900

    Link 경로 수정

--- app/challenge/header.tsx
+++ app/challenge/header.tsx
@@ -6,7 +6,7 @@ export default function ChallengeHeader() {
     return (
         <div className="flex flex-row justify-between items-center w-[100%] h-[56px] px-[16px]">
             <HeaderLogo width={"126"} height={"32"} color={"white"} />
-            <Link href={"./profile"} className="cursor-pointer">
+            <Link href={"/challenge/profile"} className="cursor-pointer">
                 <Profile width={"40"} height={"40"} />
             </Link>
         </div>
