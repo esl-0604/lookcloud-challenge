def5164bc0e102df52790c556fc70305bfc2f888
LEE <eslee850@gmail.com>
2023-09-06 16:58:39 +0900

    navBar 위치 변경으로 인한 에러 수정

--- app/lookbook/page.tsx
+++ app/lookbook/page.tsx
@@ -4,7 +4,7 @@ export default function Lookbook() {
     return (
         <main className="flex flex-col justify-center items-center w-[100%] h-[100%] bg-[#F5F5F5] text-black">
             <h1>Lookbook</h1>
-            <NavBar page={"lookbook"} />
+            {/* <NavBar page={"lookbook"} /> */}
         </main>
     );
 }
