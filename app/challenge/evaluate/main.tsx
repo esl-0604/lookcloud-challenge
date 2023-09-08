96bc6337f78b5828a32e705f0760a49860040eff
chany63 <chany63@korea.ac.kr>
2023-09-08 22:08:27 +0900

    fix: evaluate page

--- app/challenge/evaluate/main.tsx
+++ app/challenge/evaluate/main.tsx
@@ -14,139 +14,133 @@ const DEFAULT_W = 300;
 const DEFAULT_H = 450;
 
 interface ConfigState {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 
 export default function ChallengeEvaluateMain() {
-    const boundaryRef = useRef<HTMLDivElement>(null);
+  const boundaryRef = useRef<HTMLDivElement>(null);
 
-    const [{ x, y }, setConfig] = useState<ConfigState>({
-        x: 0,
+  const [{ x, y }, setConfig] = useState<ConfigState>({
+    x: 0,
+    y: 40,
+  });
+
+  useEffect(() => {
+    const boundary = boundaryRef.current?.getBoundingClientRect();
+
+    if (boundary) {
+      setConfig({
+        x: Math.floor(boundary.width / 2 - DEFAULT_W / 2),
         y: 40,
-    });
+      });
+    }
+  }, []);
 
-    useEffect(() => {
-        const boundary = boundaryRef.current?.getBoundingClientRect();
+  const [isUp, setIsUp] = useState<boolean>(false);
+  const [isDown, setIsDown] = useState<boolean>(false);
+  const [showThumbs, setShowThumbs] = useState<boolean>(false);
+  const [isInfo, setIsInfo] = useState<boolean>(false);
 
-        if (boundary) {
-            setConfig({
-                x: Math.floor(boundary.width / 2 - DEFAULT_W / 2),
-                y: 40,
-            });
-        }
-    }, []);
+  useEffect(() => {
+    if (showThumbs) {
+      const timer = setTimeout(() => {
+        setShowThumbs(false);
+        setIsInfo(true);
+      }, 1000);
+      return () => clearTimeout(timer); // 타이머 해제
+    }
+  }, [showThumbs]);
 
-    const [isUp, setIsUp] = useState<boolean>(false);
-    const [isDown, setIsDown] = useState<boolean>(false);
-    const [showThumbs, setShowThumbs] = useState<boolean>(false);
-    const [isInfo, setIsInfo] = useState<boolean>(false);
+  useEffect(() => {
+    if (isUp || isDown) setShowThumbs(true);
+    else setShowThumbs(false);
+  }, [isUp, isDown]);
 
-    useEffect(() => {
-        if (showThumbs) {
-            const timer = setTimeout(() => {
-                setShowThumbs(false);
-                setIsInfo(true);
-            }, 1000);
-            return () => clearTimeout(timer); // 타이머 해제
-        }
-    }, [showThumbs]);
+  useEffect(() => {
+    if (Math.floor(y) <= 0) setIsUp(true);
+    else if (Math.floor(y) >= 80) setIsDown(true);
+    else {
+      setIsUp(false);
+      setIsDown(false);
+    }
+  }, [y]);
 
-    useEffect(() => {
-        if (isUp || isDown) setShowThumbs(true);
-        else setShowThumbs(false);
-    }, [isUp, isDown]);
-
-    useEffect(() => {
-        if (Math.floor(y) <= 0) setIsUp(true);
-        else if (Math.floor(y) >= 80) setIsDown(true);
-        else {
-            setIsUp(false);
-            setIsDown(false);
-        }
-    }, [y]);
-
-    return (
-        <div className="p-4 w-[100%] h-[100%] flex flex-col items-center">
-            {isUp ? (
-                <div>
-                    {isInfo ? (
-                        <div>
-                            <LookInfoBox />
-                        </div>
-                    ) : (
-                        <div
-                            className="absolute top-[72px]"
-                            style={{
-                                left: "50%",
-                                transform: "translate(-50%, 0)",
-                                width: 300,
-                                height: 450,
-                            }}
-                        >
-                            <ThumbsUpBox />
-                        </div>
-                    )}
-                </div>
-            ) : isDown ? (
-                <div>
-                    {isInfo ? (
-                        <div>
-                            <LookInfoBox />
-                        </div>
-                    ) : (
-                        <div
-                            className="absolute top-[152px]"
-                            style={{
-                                left: "50%",
-                                transform: "translate(-50%, 0)",
-                                width: 300,
-                                height: 450,
-                            }}
-                        >
-                            <ThumbsDownBox />
-                        </div>
-                    )}
-                </div>
-            ) : (
-                <Boundary
-                    ref={boundaryRef}
-                    style={{ width: 300, height: "100%" }}
-                >
-                    <div
-                        style={{
-                            left: x,
-                            top: y,
-                            width: DEFAULT_W,
-                            height: DEFAULT_H,
-                        }}
-                        className="relative"
-                        {...registDragEvent((deltaX, deltaY) => {
-                            if (!boundaryRef.current) return;
-
-                            const boundary =
-                                boundaryRef.current.getBoundingClientRect();
-
-                            setConfig({
-                                x: inrange(
-                                    x + deltaX,
-                                    BOUNDARY_MARGIN,
-                                    boundary.width - DEFAULT_W - BOUNDARY_MARGIN
-                                ),
-                                y: inrange(
-                                    y + deltaY,
-                                    BOUNDARY_MARGIN,
-                                    boundary.height -
-                                        DEFAULT_H -
-                                        BOUNDARY_MARGIN
-                                ),
-                            });
-                        })}
-                    >
-                        <Box />
-                    </div>
-                </Boundary>
-            )}
+  return (
+    <div className="px-4 w-[100%] h-[100%] flex flex-col items-center">
+      {isUp ? (
+        <div>
+          {isInfo ? (
+            <div>
+              <LookInfoBox />
+            </div>
+          ) : (
+            <div
+              className="absolute top-[52px]"
+              style={{
+                left: "50%",
+                transform: "translate(-50%, 0)",
+                width: 300,
+                height: 450,
+              }}
+            >
+              <ThumbsUpBox />
+            </div>
+          )}
         </div>
-    );
+      ) : isDown ? (
+        <div>
+          {isInfo ? (
+            <div>
+              <LookInfoBox />
+            </div>
+          ) : (
+            <div
+              className="absolute top-[132px]"
+              style={{
+                left: "50%",
+                transform: "translate(-50%, 0)",
+                width: 300,
+                height: 450,
+              }}
+            >
+              <ThumbsDownBox />
+            </div>
+          )}
+        </div>
+      ) : (
+        <Boundary ref={boundaryRef} style={{ width: 300, height: "100%" }}>
+          <div
+            style={{
+              left: x,
+              top: y,
+              width: DEFAULT_W,
+              height: DEFAULT_H,
+            }}
+            className="relative"
+            {...registDragEvent((deltaX, deltaY) => {
+              if (!boundaryRef.current) return;
+
+              const boundary = boundaryRef.current.getBoundingClientRect();
+
+              setConfig({
+                x: inrange(
+                  x + deltaX,
+                  BOUNDARY_MARGIN,
+                  boundary.width - DEFAULT_W - BOUNDARY_MARGIN
+                ),
+                y: inrange(
+                  y + deltaY,
+                  BOUNDARY_MARGIN,
+                  boundary.height - DEFAULT_H - BOUNDARY_MARGIN
+                ),
+              });
+            })}
+          >
+            <Box />
+          </div>
+        </Boundary>
+      )}
+    </div>
+  );
 }
--- app/components/LookInfoBox.tsx
+++ app/components/LookInfoBox.tsx
@@ -1,25 +1,57 @@
 export default function LookInfoBox() {
-    return (
-        <div className="h-full w-full relative">
-            <img
-                src="/image/image.png"
-                alt="image"
-                style={{ width: "300px", height: "450px" }}
-            />
-
-            <div
-                className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-[#333333] to-[#333333]"
-                style={{
-                    zIndex: 10,
-                    height: "15%",
-                }}
-            >
-                <div className="absolute bottom-3 ml-2">
-                    <span className="text-left text-xs text-white">
-                        고연전을 즐기기위한 편한 무지 티를 기반으로 만든 룩
-                    </span>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col h-full w-full">
+      <div className="h-full w-full relative">
+        <img
+          src="/image/image.png"
+          alt="image"
+          style={{ width: "300px", height: "450px" }}
+        />
+        <div
+          className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-b from-transparent via-[#333333] to-[#333333]"
+          style={{
+            zIndex: 10,
+            height: "15%",
+          }}
+        >
+          <div className="absolute bottom-3 ml-2">
+            <span className="text-left text-xs text-white">
+              고연전을 즐기기위한 편한 무지 티를 기반으로 만든 룩
+            </span>
+          </div>
         </div>
-    );
+      </div>
+      <div className="flex flex-col mt-[12px]">
+        <div className="flex flex-row justify-between items-center w-[100%] h-[40px] bg-black mt-1 rounded-md px-[8px]">
+          <div className="flex flex-row">
+            <img
+              className="rounded-full"
+              src="/svg/profileImg.svg"
+              width="50px"
+              height="50px"
+              style={{ marginRight: 4 }}
+            />
+            <div className="flex flex-col py-[8px]">
+              <div className="flex flex-row">
+                <span className="text-white text-[8px]">나는야이은상</span>
+              </div>
+              <span className="text-white text-[8px] leading-[8px]">
+                @eslee850
+              </span>
+            </div>
+          </div>
+          <span className="text-white text-[24px]">점수</span>
+        </div>
+        <div className="flex flex-col mt-[12px]">
+          <span className="text-white text-[12px]">
+            상의 - musinsa standard basic cotton t-shirts
+          </span>
+          <span className="text-white text-[12px]">
+            하의 - musinsa standard banding chino pants
+          </span>
+          <span className="text-white text-[12px]">신발 - 정보 없음</span>
+        </div>
+      </div>
+    </div>
+  );
 }
