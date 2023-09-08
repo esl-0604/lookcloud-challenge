8cde0e47a8f0c517d46448b45d818b363052a089
LEE <eslee850@gmail.com>
2023-09-09 04:00:50 +0900

    conflict 해결

--- app/challenge/leaderboard/page.tsx
+++ app/challenge/leaderboard/page.tsx
@@ -2,10 +2,10 @@ import ChallengeHeader from "../header";
 import ChallengeLeaderBoardMain from "./main";
 
 export default function ChallengeLeaderBoard() {
-  return (
-    <main className="flex flex-col justify-start items-center absolute w-[100%] min-h-[100%]">
-      <ChallengeHeader />
-      <ChallengeLeaderBoardMain />
-    </main>
-  );
+    return (
+        <main className="flex flex-col justify-start items-center absolute w-[100%] min-h-[100%]">
+            <ChallengeHeader />
+            <ChallengeLeaderBoardMain />
+        </main>
+    );
 }
