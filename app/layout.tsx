d74ccb487a0a67105a7a22b254488c1bfad9c66a
esl-0604 <eslee850@gmail.com>
2023-09-19 19:35:48 +0900

    brouser refresh ban_3

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 				</head>
 
 				<body>
-					<div className="flex justify-center items-center w-screen h-screen overscroll-contain">
+					<div className="flex justify-center items-center w-screen h-screen overscroll-none">
 						<div className="relative w-[100%] h-[100%] max-w-[480px] select-none">
 							{children}
 						</div>
