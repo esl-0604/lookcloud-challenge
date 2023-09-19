62c560cf163457413b1a98e7fb03d1d04b2bbd67
esl-0604 <eslee850@gmail.com>
2023-09-20 01:38:49 +0900

    gallery

--- app/service/challenge/participate/uploadbutton.tsx
+++ app/service/challenge/participate/uploadbutton.tsx
@@ -63,7 +63,7 @@ export default function ChallengeParticipantUploadButton({
 				className="hidden"
 				type="file"
 				accept="image/*"
-				capture="environment"
+				capture={true}
 				onChange={handleChange}
 			/>
 		</form>
