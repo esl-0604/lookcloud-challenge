5ef23b15c3d92b25f1505f843f54ed5df463d687
esl-0604 <eslee850@gmail.com>
2023-09-25 16:19:25 +0900

    nav bar feedback 빌드 에러 제거

--- app/service/feedback/page.tsx
+++ app/service/feedback/page.tsx
@@ -4,7 +4,7 @@ export default function Feedback() {
 	return (
 		<main className="flex flex-col justify-center items-center w-[100%] h-[100%] bg-[#F5F5F5] text-black">
 			<h1>Feedback</h1>
-			<NavBar page={"feedback"} />
+			{/* <NavBar page={"feedback"} /> */}
 		</main>
 	)
 }
